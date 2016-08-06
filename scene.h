29dfcb92667f12eaecc245bbff77f4f2ed8b8638
cedarz <dyingthesun@gmail.com>
2016-08-06 08:55:34 +0800

    scene

--- scene.h
+++ scene.h
@@ -1,1 +1,22 @@
 // scene data structure
+#ifndef _SCENE_H_
+#define _SCENE_H_
+#include "vec3.h"
+
+namespace yart {
+	struct Camera {
+		vec3 position;
+		vec3 center; // center of interest, or view direction
+		float angle; // view angle
+		float aspect; // aspect ratio
+		float hither, yon; // clipping distances
+		float tilt; // tilt angle
+		float xres, yres; // x and y resolution
+	};
+
+	struct sphere {
+
+	};
+}
+
+#endif /*endf of _SCENE_H_*/
